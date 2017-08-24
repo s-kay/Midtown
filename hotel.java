34dfd396b8a964f6429816c194bbb873d7552090
s-kay <kimani.stan.ks@gmail.com>
2017-08-24 16:01:38 +0300

    committed ,java and .md files

---
+++ hotel.java
@@ -0,0 +1,12 @@
+public class Hotel {
+	String room;
+
+	public void checkRoom{
+		if (room == true)
+			System.out.println("Rooms available")
+
+		else {
+			System.out.println("No rooms available")
+			}
+		}	
+}
